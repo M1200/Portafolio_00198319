7a5ea6401cd23e674ab3f9d15c00d7ba24490108
M1200 <54820873+M1200@users.noreply.github.com>
2019-09-07 11:41:00 -0600

    Ejercicios02 y19 completos

---
+++ Laboratorios/Laboratorio1/Guía de laboratorio 01.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Ejercicio no. 2
+/*void raizCuadrada(double n, double r){
+    float NewR=((n/r)+r)/2;
+     if(abs(r-NewR)<0.001)  {
+           cout<<NewR;
+           return;
+         }
+        else{
+            return raizCuadrada(n,NewR);
+            }
+    }
+
+
+int main(){
+  double  n;
+  double r=1;
+
+  cout<< "Ingrese el numero de que desea conocer la raiz cuadrada: ";
+  cin>> n;
+
+  raizCuadrada(n,r);
+
+
+  return 0;
+
+}*/
+
+//Ejercicio 19
+/*int main(){
+	int matriz[5][5] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+
+	cout<<"Mostrando Matriz completa\n";
+
+
+
+	for(int i=0;i<5;i++){
+		for(int j=0;j<=i;j++){
+         cout<<"Digite el valor deseado: ";
+         cin>> matriz[i][j];
+		}
+		cout<<"\n";
+
+	}
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+         cout<< matriz[i][j];
+		}
+		cout<< endl;
+	}
+}*/
+
+/*void conversorHexa(int nu){
+    if ()
+
+
+}
+
+
+//Ejercicio 24*/
+
+struct Suma []{
+
+
+};
+
+
+
