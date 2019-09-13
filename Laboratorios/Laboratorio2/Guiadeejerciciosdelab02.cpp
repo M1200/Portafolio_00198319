6eed469fe24d426b2d180222e25f44994de752b0
M1200 <54820873+M1200@users.noreply.github.com>
2019-09-12 19:56:21 -0600

    Ejercicio 3d
    Incompleto, subir el resto

---
+++ Laboratorios/Laboratorio2/Guiadeejerciciosdelab02.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int acum=0;
+
+
+int sumatoria (int x, int k){
+
+
+
+}
+
+int main(){
+int x=0;
+char resp;
+
+cout<< "Ingrese el valor de X: ";
+cin>> x;
+
+cout<< "Desea continuar? (s/n)"<< endl;
+cin>> resp;
+
+while (resp=='s'){
+   cout<< "Ingrese el valor de X: ";
+   cin>> x;
+
+
+    cout<<"Desea continuar? (s/n)"<< endl;
+    cin>> resp;
+
+
+}
+return 0;
+}
